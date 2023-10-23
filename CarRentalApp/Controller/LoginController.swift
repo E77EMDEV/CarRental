b9388df3657ffffe931bfbb25fd0fbb5a55c340d
E77EMDEV <qurbangulmammadov@yahoo.com>
2023-10-23 23:04:17 +0400

    Tabbar

--- CarRentalApp/Controller/LoginController.swift
+++ CarRentalApp/Controller/LoginController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +16,6 @@ class ViewController: UIViewController {
         
     }
 
+    
 }
 
